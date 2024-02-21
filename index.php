37407b7c5a0da4287576c723864a277e95a7449a
sulthony.akbar <sulthonyakbar18@gmail.com>
2024-02-21 16:01:17 +0700

    upsell page

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer"   />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Upsell Page</title>
+</head>
+
+<body>
+    <div class="parallax d-flex justify-content-center align-items-center p-5">
+        <div class="container-fluid">
+            <h1 id="header-warning" class="text-white text-center fw-bold fst-italic mb-5">Upgrade To The <span class="text-warning">"EXTENDED Pack"</span></h1>
+            <h1 id="header-title" class="text-white text-center fw-bold">DOUBLE Your Package by Grabing This Fabulous Upgrade Pack with <span class="text-warning">"Un-Restricted" PLR License </span>to Make You Able to Double The Profits Without Any Restriction!!</h1>
+        </div> 
+    </div>
+
+    <div class="container-fluid vh-100 p-3">
+        <h1 class="text-center m-5">You'll Receive Extended PLR License for Both Main Product + This Upgrade Pack</h1>
+
+        <img src="" alt="">
+
+        <div class="container">
+            <div class="row mb-3 justify-content-center">
+                <a class="btn btn-secondary me-5 p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> BIGGER Content Collection</a>
+                <a class="btn btn-secondary p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> Editable Design Templates</a>
+            </div>
+            <div class="row mb-3 justify-content-center">
+                <a class="btn btn-secondary me-5 p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> PLR Right for FE + Upgrade Pack</a>
+                <a class="btn btn-secondary p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> Easy to edit and customize</a>
+            </div>
+            <div class="row mb-3 justify-content-center">
+                <a class="btn btn-secondary me-5 p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> Complete reselling toolkits</a>
+                <a class="btn btn-secondary p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> zero design skill needed</a>
+            </div>
+            <div class="row mb-3 justify-content-center">
+                <a class="btn btn-secondary me-5 p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> UNLIMITED Clients Services</a>
+                <a class="btn btn-secondary p-3 fw-bold rounded-pill" href="#" role="button" style="width: 399px;"><i class="fas fa-check-circle"></i> save time money & effort</a>
+            </div>
+        </div>
+
+        <h1 class="text-center m-5">Grab Adult Coloring "EXTENDED Pack" And Start Make MORE Money Today!</h1>
+    </div>
+
+    <h3 class="text-center m-5">Receive "Un-Restricted" PLR License To FE + Upgrade Pack!</h3>
+    <div class="container d-flex align-items-center justify-content-center">
+        <a class="btn btn-secondary p-3 fw-bold fs-5 rounded-pill" href="#" role="button" style="width: 599px;">Get Instant Access Now</a>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-md-6 me-5">
+                <h4>As my brand new valuable customer, I’d like to offer you an outrageous 45% discount for the Adult Coloring Books “EXTENDED” Upgrade Pack!.
+                    With this EXTENDED upgrade, you can get additional Stunning and Pro-Looking Templates and take your viral marketing to the next level.
+                    This exclusive upgrade consists of 3 key profit boosters:</h4>
+            </div>
+            <div class="col-md-5">
+                <img src="img/23672403_6803712.svg" alt="">
+            </div>
+
+            <h4 class="text-center mt-5">This Outrageous Offer May Changed Any Time Please... Pay Very Close ATTENTION!</h4>
+        </div>
+    </div>
+
+    <h4 class="text-center mt-5">Let’s Go Through Each One. You’ll Be Blown Away!</h4>
+
+    <div class="container mt-5">
+        <div class="d-flex align-items-center justify-content-center">
+            <a class="btn btn-secondary p-3 fw-bold fs-5 rounded-pill" href="#" role="button" style="width: 599px;">BOOSTER #1 : "Un-Restricted" PLR License</a>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row d-flex align-items-center justify-content-center">
+            <h4 class="text-center mb-5">"Un-Restricted" PLR License</h4>
+            <div class="col-md-5">
+                <img src="img/23672403_6803712.svg" alt="">
+            </div>
+            <div class="col-md-6 me-5">
+                <h4>As my brand new valuable customer, I’d like to offer you an outrageous 45% discount for the Adult Coloring Books “EXTENDED” Upgrade Pack!.
+                    With this EXTENDED upgrade, you can get additional Stunning and Pro-Looking Templates and take your viral marketing to the next level.
+                    This exclusive upgrade consists of 3 key profit boosters:</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="d-flex align-items-center justify-content-center">
+            <a class="btn btn-secondary p-3 fw-bold fs-5 rounded-pill" href="#" role="button" style="width: 599px;">BOOSTER #2 : Extra Creative Quotes Designs</a>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row d-flex align-items-center justify-content-center">
+            <h4 class="text-center mb-5">690+ Extra Coloring Pages</h4>
+            <div class="col-md-6 me-5">
+                <h4>As my brand new valuable customer, I’d like to offer you an outrageous 45% discount for the Adult Coloring Books “EXTENDED” Upgrade Pack!.
+                    With this EXTENDED upgrade, you can get additional Stunning and Pro-Looking Templates and take your viral marketing to the next level.
+                    This exclusive upgrade consists of 3 key profit boosters:</h4>
+            </div>
+            <div class="col-md-5">
+                <img src="img/23672403_6803712.svg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <h3 class="text-center mt-5">This Is 100% DIFFERENT From The Previous Package It's A BIGGER And More Varied Collection Of Designs</h3>
+        <div class="row justify-content-center mt-5">
+            <span class="border border-black fw-bold fs-5 text-center p-3 rounded-pill me-5" style="width: 299px;">15 Different Categories!</span>
+            <span class="border border-black fw-bold fs-5 text-center p-3 rounded-pill ms-5" style="width: 299px;">690+ Creative Designs!</span>
+        </div>
+    </div>
+
+    <h4 class="text-center">Let Me Show You What You'll Get Inside This Special "EXTENDED" Business Pack!</h4>
+
+    <div class="container mt-5">
+        <div class="d-flex align-items-center justify-content-center">
+            <a class="btn btn-secondary p-3 fw-bold fs-5 rounded-pill" href="#" role="button" style="width: 599px;">BOOSTER #3 : Completes Re-Seller Kit</a>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row d-flex align-items-center justify-content-center mb-5 align-items-stretch">
+            <div class="col-md-5 rounded-5 p-5 me-3" style="background-color: #d9d9d9;">
+                <h2 class="text-center mb-3">Landing Page</h2>
+                <div class="d-flex justify-content-center">
+                    <img src="img/outline-man-web-designer-creating-landing-page-1.png" alt="" class="justify-content-center">
+                </div>
+                <p class="text-center fs-5 fw-semibold">We have professionally designed the Sales Pages for you to use them for your sales funnel.These DFY landing page templates are ready to import and use to improvise your sales.</p>
+            </div>
+            <div class="col-md-5 rounded-5 p-5 ms-3" style="background-color: #d9d9d9;">
+                <h2 class="text-center mb-3">Marketing Graphics</h2>
+                <div class="d-flex justify-content-center">
+                    <img src="img/lounge-marketing-analytics-tools-on-laptop.png" alt="" class="justify-content-center">
+                </div>
+                <p class="text-center fs-5 fw-semibold">We have professionally designed the Sales Pages for you to use them for your sales funnel.These DFY landing page templates are ready to import and use to improvise your sales.</p>
+            </div>
+        </div>
+        <div class="row d-flex align-items-center justify-content-center mb-5 align-items-stretch">
+            <div class="col-md-5 rounded-5 p-5 me-3" style="background-color: #d9d9d9;">
+                <h2 class="text-center mb-3">Sales Copy</h2>
+                <div class="d-flex justify-content-center">
+                    <img src="img/neon-woman-presenting-a-new-product-in-a-video-advertisement.png" alt="" class="justify-content-center">
+                </div>
+                <p class="text-center fs-5 fw-semibold">We have professionally designed the Sales Pages for you to use them for your sales funnel.These DFY landing page templates are ready to import and use to improvise your sales.</p>
+            </div>
+            <div class="col-md-5 rounded-5 p-5 ms-3" style="background-color: #d9d9d9;">
+                <h2 class="text-center mb-3">Email Marketing</h2>
+                <div class="d-flex justify-content-center">
+                    <img src="img/indigo-email-marketing.png" alt="" class="justify-content-center">
+                </div>
+                <p class="text-center fs-5 fw-semibold">We have professionally designed the Sales Pages for you to use them for your sales funnel.These DFY landing page templates are ready to import and use to improvise your sales.</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
